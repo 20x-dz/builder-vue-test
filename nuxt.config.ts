c8475390944229c8c1204df340009d0c0f2cffa3
20x-dz <113644268+20x-dz@users.noreply.github.com>
2024-01-19 20:35:18 +0100

    build: bump @builder.io/sdk-vue to v0.11.2
    as this fixes the CSS and symbol issues
    
    * https://github.com/BuilderIO/builder/issues/2917
    * https://github.com/BuilderIO/builder/issues/2925

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  build: {
-    /**
-     * You need to transpile the Builder.io Vue SDK for it to work correctly.
-     */
-    transpile: ['@builder.io/sdk-vue'],
-  },
 })
