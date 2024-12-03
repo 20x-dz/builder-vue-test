657922e1d472f4097d5e38d1803f641c5bb2b15f
20x-dz <113644268+20x-dz@users.noreply.github.com>
2024-12-03 13:10:01 +0100

    feat: add custom schemaOrg stuff

--- app.vue
+++ app.vue
@@ -5,3 +5,32 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+useSchemaOrg([
+  defineOrganization({
+    "@type": "MedicalOrganization",
+    address: {
+      addressCountry: "AT",
+      addressLocality: "Wien",
+      addressRegion: "Wien",
+      postalCode: "1234",
+      streetAddress: "Musterstrasse 1234",
+    },
+    description: "Description",
+    isAcceptingNewPatients: true,
+    logo: "https://upload.wikimedia.org/wikipedia/commons/9/9c/Dunder_Mifflin%2C_Inc.svg",
+    medicalSpecialty: "http://schema.org/PrimaryCare",
+    name: "ACME Corp",
+    sameAs: [
+      "https://example.com",
+    ],
+    slogan: "Limitless Paper in a Paperless World",
+  }),
+  defineWebSite({
+    name: "ACME Corp",
+  }),
+  defineWebPage(),
+]);
+</script>
+
